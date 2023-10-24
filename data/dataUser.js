fcfb475a0f430cb411e434f0b7cf0b477ef144a4
FirdausAgil <tweeknd21@gmail.com>
2023-10-24 22:25:13 +0700

    first commit

---
+++ data/dataUser.js
@@ -0,0 +1,22 @@
+export const userData = [
+    {
+        username: 'agil',
+        password: 'agil123',
+        imgUrl: 'https://randomuser.me/api/portraits/men/1.jpg'
+    },
+    {
+        username: 'ridho',
+        password: 'ridho123',
+        imgUrl: 'https://randomuser.me/api/portraits/men/41.jpg'
+    },
+    {
+        username: 'sam',
+        password: 'sam123',
+        imgUrl: 'https://randomuser.me/api/portraits/men/46.jpg'
+    },
+    {
+        username: 'dhenmas',
+        password: 'dhenmas123',
+        imgUrl: 'https://randomuser.me/api/portraits/men/32.jpg'
+    }
+]
