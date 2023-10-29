9461e2abf41aa9fc8fecb7b501a8113f24a6b03c
Ridho-Chaniago <ridho12c.rc@gmail.com>
2023-10-29 15:45:59 +0700

    update

---
+++ data/productList.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    image: "../../src/img/productPicture/apple.png",
+    name: "Red Apple",
+    weight: "1.00Kg",
+    price: "$4.99",
+  },
+  {
+    image: "../../src/img/productPicture/alpukat.jpg",
+    name: "Avocado",
+    weight: "1.00Kg",
+    price: "$2.45",
+  },
+  {
+    image: "../../src/img/productPicture/anggurmerah.jpg",
+    name: "Grape",
+    weight: "1.00Kg",
+    price: "$6.35",
+  },
+  {
+    image: "../../src/img/productPicture/buahnaga.jpg",
+    name: "Dragon Fruit",
+    weight: "1.00Kg",
+    price: "$6.45",
+  },
+  {
+    image: "../../src/img/productPicture/Durian.jpg",
+    name: "Durian",
+    weight: "1.00Kg",
+    price: "$12.95",
+  },
+  {
+    image: "../../src/img/productPicture/jeruk.jpg",
+    name: "Orange",
+    weight: "1.00Kg",
+    price: "$1.25",
+  },
+  {
+    image: "../../src/img/productPicture/mango.jpg",
+    name: "Mango",
+    weight: "1.00Kg",
+    price: "$3.25",
+  },
+  {
+    image: "../../src/img/productPicture/nanas.jpg",
+    name: "Pinapple",
+    weight: "1.00Kg",
+    price: "$2.85",
+  },
+  {
+    image: "../../src/img/productPicture/nangka.jpg",
+    name: "Jackfruit",
+    weight: "1.00Kg",
+    price: "$1.00",
+  },
+  {
+    image: "../../src/img/productPicture/pisang.jpg",
+    name: "Banana",
+    weight: "1.00Kg",
+    price: "$1.25",
+  },
+  {
+    image: "../../src/img/productPicture/semangka.jpg",
+    name: "Watermelon",
+    weight: "1.00Kg",
+    price: "$2.25",
+  },
+];
+
+export default products;
